85769eb1c39109aa37ba835b5c8ada06531de4f1
dhruvarayasam <dhruva.rayasam@gmail.com>
2022-03-29 08:19:07 -0400

    established main structure of app: enhanced simplicity and reduced dependence on main component interaction, instead implemented sub-components | also improved CSS

---
+++ src/components/Stats.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Stats() {
+    function Timer() {
+
+    }
+
+    function EfficiencyMeter() {
+
+    }
+
+    return (
+        <div>
+            <Timer/>
+            <EfficiencyMeter/>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApplicationWindow from './components/ApplicationWindow'
+import '../src/styles/index.css'
 //Render all components here in their respective divs
 
-
+ReactDOM.render(<ApplicationWindow/>, document.getElementById("root"))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
